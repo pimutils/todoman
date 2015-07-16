1d9d93940cd703cf29369417b8275d0eb8d88e83
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-07-16 19:10:25 +0200

    Add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ import pytest
 @pytest.fixture
 def config(tmpdir):
     path = tmpdir.join('config')
+    tmpdir.mkdir('default')  # default calendar
     path.write('[main]\n'
                'path = {}/*\n'
                'date_format = %Y-%m-%d\n'
