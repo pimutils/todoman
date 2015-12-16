1e13322c1155af2f8886ac6459fb5f84345b7a58
Hugo Osvaldo Barrera <hugo@barrera.io>
2015-12-16 19:09:50 -0300

    Add some more unit tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -20,6 +20,50 @@ def test_basic(tmpdir, runner):
     assert 'harhar' in result.output
 
 
+def test_percent(tmpdir, runner):
+    tmpdir.join('default/test.ics').write(
+        'BEGIN:VCALENDAR\n'
+        'BEGIN:VTODO\n'
+        'SUMMARY:harhar\n'
+        'PERCENT-COMPLETE:78\n'
+        'END:VTODO\n'
+        'END:VCALENDAR'
+    )
+    result = runner.invoke(cli, ['list'])
+    assert not result.exception
+    assert '78%' in result.output
+
+
+def test_show_existing(tmpdir, runner):
+    tmpdir.join('default/test.ics').write(
+        'BEGIN:VCALENDAR\n'
+        'BEGIN:VTODO\n'
+        'SUMMARY:harhar\n'
+        'DESCRIPTION:Lots of text. Yum!\n'
+        'END:VTODO\n'
+        'END:VCALENDAR'
+    )
+    result = runner.invoke(cli, ['list'])
+    result = runner.invoke(cli, ['show', '1'])
+    assert not result.exception
+    assert 'harhar' in result.output
+    assert 'Lots of text. Yum!' in result.output
+
+
+def test_show_inexistant(tmpdir, runner):
+    tmpdir.join('default/test.ics').write(
+        'BEGIN:VCALENDAR\n'
+        'BEGIN:VTODO\n'
+        'SUMMARY:harhar\n'
+        'END:VTODO\n'
+        'END:VCALENDAR'
+    )
+    result = runner.invoke(cli, ['list'])
+    result = runner.invoke(cli, ['show', '2'])
+    assert result.exit_code == -2
+    assert result.output == 'No todo with id 2.\n'
+
+
 def test_human(runner):
     result = runner.invoke(cli, [
         'new', '-l', 'default', '-d', 'tomorrow', 'hail belzebub'
