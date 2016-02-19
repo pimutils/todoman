c35bb29042ec38598894e75ae10371059f621974
Hugo Osvaldo Barrera <hugo@barrera.io>
2016-02-18 23:30:33 -0300

    Update licence year

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Todoman'
-copyright = '2015, Hugo Osvaldo Barrera'
+copyright = '2015-2016, Hugo Osvaldo Barrera'
 author = 'Hugo Osvaldo Barrera'
 
 # The version info for the project you're documenting, acts as replacement for
