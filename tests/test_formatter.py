8de9212efd16ac2e4998e8e7ffea248eacbafea2
Rimsha Khan <rimshakhan.rk03@gmail.com>
2017-02-21 23:41:14 +0530

    Chnage date to a datetime object while testing formatter

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -1,20 +1,21 @@
 from datetime import datetime, timedelta
 from random import randrange
+
 from todoman import ui
 
 DATE_FORMAT = "%d-%m-%y"
+
 
 def test_format_date():
     """
     Tests the format_date function in todoman.ui.TodoFormatter
     """
     formatter = ui.TodoFormatter(DATE_FORMAT)
-    today = datetime.now().date()
+    date_width = formatter.date_width
+    today = datetime.now()
     tomorrow = today + timedelta(days = 1)
-    any_day = today + datetime.timedelta(days = random.randrange(2))
+    any_day = today + timedelta(days = randrange(2, 8))
 
-    str_format = lambda x: x.rjust((formatter.date_width), " ")
-
-    assert formatter.format_date(today) == str_format("Today")
-    assert formatter.format_date(tomorrow) == str_format("Tomorrow")
+    assert formatter.format_date(today) == "Today".rjust(date_width)
+    assert formatter.format_date(tomorrow) == "Tomorrow".rjust(date_width)
     assert formatter.format_date(any_day) == any_day.strftime(DATE_FORMAT)
