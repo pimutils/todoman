2f74ac3291df71c5de8ead2822cf007b6b806004
Rimsha Khan <rimshakhan.rk03@gmail.com>
2017-02-22 00:19:48 +0530

    Hard code the test case to notify us of any change

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -11,11 +11,10 @@ def test_format_date():
     Tests the format_date function in todoman.ui.TodoFormatter
     """
     formatter = ui.TodoFormatter(DATE_FORMAT)
-    date_width = formatter.date_width
     today = datetime.now()
     tomorrow = today + timedelta(days = 1)
     any_day = today + timedelta(days = randrange(2, 8))
 
-    assert formatter.format_date(today) == "Today".rjust(date_width)
-    assert formatter.format_date(tomorrow) == "Tomorrow".rjust(date_width)
+    assert formatter.format_date(today) == "   Today"
+    assert formatter.format_date(tomorrow) == "Tomorrow"
     assert formatter.format_date(any_day) == any_day.strftime(DATE_FORMAT)
