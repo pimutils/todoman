93a49c531a308c00e4169b5bbab2e752f0a67c1c
Hugo Osvaldo Barrera <hugo@barrera.io>
2017-02-21 11:31:51 -0300

    Show links to github/travis in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -191,7 +191,13 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#  html_theme_options = {}
+html_theme_options = {
+    'github_user': 'pimutils',
+    'github_repo': 'todoman',
+    'travis_button': 'true',
+    'github_banner': 'true',
+    'github_button': 'false',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #  html_theme_path = []
