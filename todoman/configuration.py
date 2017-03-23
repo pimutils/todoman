f75b0985a9976f6d05096ac182789c1d99844a8e
Hugo Osvaldo Barrera <hugo@barrera.io>
2017-03-22 22:54:04 -0300

    Remove unused, broken function

--- todoman/configuration.py
+++ todoman/configuration.py
@@ -3,7 +3,7 @@ from os.path import exists, join
 
 import xdg.BaseDirectory
 from configobj import ConfigObj, flatten_errors
-from validate import is_option, Validator, VdtValueError
+from validate import Validator, VdtValueError
 
 from todoman import __documentation__
 
@@ -47,15 +47,6 @@ def validate_time_format(fmt):
             'for that.'
         )
     return fmt
-
-
-def validate_color(color):
-    color = is_option(color)
-    if color == 'always':
-        return True
-    elif color == 'never':
-        return False
-    return None
 
 
 def find_config():
