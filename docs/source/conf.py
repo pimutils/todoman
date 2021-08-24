0ad6604d9f947ebbdbf9ad7c5e253a1bc3d2e1d4
Hugo Osvaldo Barrera <hugo@barrera.io>
2021-08-24 19:23:54 +0200

    Change docs theme to RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,6 +64,7 @@ extensions = [
     "sphinx_autorun",
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -142,17 +143,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "alabaster"
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-html_theme_options = {
-    "github_user": "pimutils",
-    "github_repo": "todoman",
-    "github_banner": "true",
-    "github_button": "false",
-}
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom themes here, relative to this directory.
 #  html_theme_path = []
