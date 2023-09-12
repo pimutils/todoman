0834d0b038420b2f5c527e434661fab51075a768
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2023-09-12 11:23:03 +0200

    Add type hints for doc config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,7 @@
 # noqa: INP001
 
+from typing import Iterator
+
 import todoman
 from todoman.configuration import CONFIG_SPEC
 from todoman.configuration import NO_DEFAULT
@@ -7,7 +9,7 @@ from todoman.configuration import NO_DEFAULT
 # -- Generate confspec.rst ----------------------------------------------
 
 
-def confspec_rst():
+def confspec_rst() -> Iterator[str]:
     """Generator that returns lines for the confspec doc page."""
 
     for name, type_, default, description, _validation in sorted(CONFIG_SPEC):
