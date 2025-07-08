b7a8bf70ae08df7ec069f343e5731e2e148842a5
David Edmondson <dme@dme.org>
2025-07-08 10:49:41 +0100

    Porcelain (JSON) output should include list colours, if any

--- todoman/formatters.py
+++ todoman/formatters.py
@@ -304,6 +304,7 @@ class PorcelainFormatter(DefaultFormatter):
             "due": self.format_datetime(todo.due),
             "id": todo.id,
             "list": todo.list.name if todo.list else None,
+            "list_colour": todo.list.colour if todo.list else None,
             "percent": todo.percent_complete,
             "summary": todo.summary,
             "categories": todo.categories,
